897842dab2c173c04c4528aa9e62fad20acbd1a7
Savka Oleg <savkaoleg1996@gmail.com>
2021-09-27 12:41:37 +0300

    Update test to duration 2m

--- generator/test.js
+++ generator/test.js
@@ -4,21 +4,34 @@ const { test, expect } = require('@playwright/test');
 const { describe, beforeEach } = test;
 const it = test;
 const xit = test;
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const sleepTimeout = 2*60*1000 / 5; // 2 min to 5 calls
 
 describe('Login', () => {
   beforeEach(async ({ page }) => {
     // console.log('beforeEach do teste');
-    await page.goto('https://front.serverest.dev')
+    await page.goto('https://front.serverest.dev');
+    await sleep(sleepTimeout);
   })
 
   it('shows error message when password is not set', async ({ page }) => {
     // console.log('preenchendo email');
     await page.type(getDataTestIdSelector('email'), 'fulano@qa.com')
+    await sleep(sleepTimeout);
+    
     // console.log('preenchendo senha');
-    await page.type(getDataTestIdSelector('senha'), 'teste')
+    await page.type(getDataTestIdSelector('senha'), 'teste');
+    await sleep(sleepTimeout);
+
     // console.log('clicando entrar');
-    await page.click(getDataTestIdSelector('entrar'))
+    await page.click(getDataTestIdSelector('entrar'));
+    await sleep(sleepTimeout);
+  
     // console.log('esperando \'logout\'');
-    await page.waitForSelector(getDataTestIdSelector('logout'))
+    await page.waitForSelector(getDataTestIdSelector('logout'));
+    await sleep(sleepTimeout);
   })
 })
